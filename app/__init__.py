a906cd0c9b5f07f5a3aa309889e4c0eb643df571
Niel <nieldv@gmail.com>
2018-10-24 01:10:11 +0200

    fixed functions not in init file

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, request
 
-from functions import ugly_load_to_sftp, ugly_load_to_db
+from functions import ugly_load_to_sftp, ugly_load_to_db,ugly_load_to_db_stops,ugly_load_to_db_om
 
 
 def create_app(test_config=None):
