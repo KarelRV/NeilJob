2632d27b51a9e4310e4475098de13507c7393ff4
Niel <nieldv@gmail.com>
2018-11-08 09:00:33 +0200

    added OM stuff

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,16 @@ def create_app(test_config=None):
         ugly_load_to_sftp()
         return "loaded to sftp"
 
+    @app.route('/load_to_sftp_om')
+    def load_to_sftp_om():
+        ugly_load_to_sftp_om()
+        return "loaded to sftp om"
+		
+    @app.route('/load_to_sftp_stops_om')
+    def load_to_sftp_stops_om():
+        ugly_load_to_sftp_om()
+        return "loaded to sftp stops om"
+		
     @app.route('/load_to_db/', methods=['GET'])
     def load_to_db():
 
@@ -100,5 +110,27 @@ def create_app(test_config=None):
         ugly_load_to_db_om(FIRSTNAME, LASTNAME, INITIAL, IDNUMBER, POSTALCODE, FROM, EMAIL, REPLYMESSAGE, ORIGINALMESSAGE,
                         ALTCONTACTNUM, DATEOFBIRTH, CAMPAIGNID, CAMPAIGNNAME, SMSSENTTIME, SMSREPLYTIME)
         return "loaded to db old mutual"
+		
+    @app.route('/load_to_db_stops_om/', methods=['GET'])
+    def load_to_db_stops_om():
+
+        FIRSTNAME = request.args.get('FIRSTNAME')
+        LASTNAME = request.args.get('LASTNAME')
+        INITIAL = request.args.get('INITIAL')
+        IDNUMBER = request.args.get('IDNUMBER')
+        POSTALCODE = request.args.get('POSTALCODE')
+        FROM = request.args.get('FROM')
+        EMAIL = request.args.get('EMAIL')
+        REPLYMESSAGE = request.args.get('REPLYMESSAGE')
+        ORIGINALMESSAGE = request.args.get('ORIGINALMESSAGE')
+        ALTCONTACTNUM = request.args.get('ALTCONTACTNUM')
+        DATEOFBIRTH = request.args.get('DATEOFBIRTH')
+        CAMPAIGNID = request.args.get('CAMPAIGNID')
+        CAMPAIGNNAME = request.args.get('CAMPAIGNNAME')
+        SMSSENTTIME = request.args.get('SMSSENTTIME')
+        SMSREPLYTIME = request.args.get('SMSREPLYTIME')
+        ugly_load_to_db_om(FIRSTNAME, LASTNAME, INITIAL, IDNUMBER, POSTALCODE, FROM, EMAIL, REPLYMESSAGE, ORIGINALMESSAGE,
+                        ALTCONTACTNUM, DATEOFBIRTH, CAMPAIGNID, CAMPAIGNNAME, SMSSENTTIME, SMSREPLYTIME)
+        return "loaded to db om stops"		
 		
     return app
